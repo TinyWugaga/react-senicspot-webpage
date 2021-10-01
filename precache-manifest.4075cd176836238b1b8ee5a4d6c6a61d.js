019285a34a5080f9f857a1fdf62b5b50ee4616fc
Tiny <405401372@gapp.fju.edu.tw>
2021-10-01 18:05:42 +0800

    Updates

---
+++ precache-manifest.4075cd176836238b1b8ee5a4d6c6a61d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4112d0a3cc136a53ba5fd59a3cca8ac",
+    "url": "/react-senicspot-webpage/index.html"
+  },
+  {
+    "revision": "b303b6bba54065848d98",
+    "url": "/react-senicspot-webpage/static/css/main.a158a356.chunk.css"
+  },
+  {
+    "revision": "de4821ca6c2990a062c5",
+    "url": "/react-senicspot-webpage/static/js/2.0bdd9b7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-senicspot-webpage/static/js/2.0bdd9b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b9a9bdb968239a7708",
+    "url": "/react-senicspot-webpage/static/js/3.8560e17d.chunk.js"
+  },
+  {
+    "revision": "b303b6bba54065848d98",
+    "url": "/react-senicspot-webpage/static/js/main.b6b7eaef.chunk.js"
+  },
+  {
+    "revision": "5f98537939a6d898a86a",
+    "url": "/react-senicspot-webpage/static/js/runtime-main.b0c74ae9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83c994f515299659a703aec84dcb7c17.js"
+  "/react-senicspot-webpage/precache-manifest.4075cd176836238b1b8ee5a4d6c6a61d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-senicspot-webpage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
